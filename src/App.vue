c11b7a6bb1311e943a34f6db395059f1e1e9d1ac
Azhar Botbayeva <azharbotbayeva@Azhars-MacBook-Pro.local>
2024-01-05 16:30:47 +0000

    feat:transaction, form validation, success/error toast

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,72 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total"/>
-    <IncomeExpenses :income="income" :expenses="expenses"/>
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
 <script setup>
-import Header from './components/Header.vue';
-import Balance from './components/Balance.vue';
-import IncomeExpenses from './components/IncomeExpenses.vue';
-import TransactionList from './components/TransactionList.vue';
-import AddTransaction from './components/AddTransaction.vue';
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+import { useToast } from 'vue-toastification'
 import { ref, computed } from 'vue'
+
+const toast = useToast();
+
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -19.99 },
   { id: 2, text: 'Salary', amount: 299.99 },
   { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 },
-]);
+  { id: 4, text: 'Camera', amount: 150 }
+])
 
 // Compute total amount
 const total = computed(() => {
-  return transactions.value.reduce((acc, transaction) => {
-    return acc + transaction.amount
-  }, 0).toFixed(2) 
+  return transactions.value
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+    .toFixed(2)
 })
 
 // Compute Income
 const income = computed(() => {
-  return transactions.value.filter((transaction)=>transaction.amount>0).reduce((acc, transaction) => {
-    return acc + transaction.amount
-  }, 0)
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
 })
 // Compute Expenses
 const expenses = computed(() => {
-  return transactions.value.filter((transaction) => transaction.amount < 0).reduce((acc, transaction) => {
-    return acc + transaction.amount
-  }, 0).toFixed(2)
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+    .toFixed(2)
 })
 
+// Add Transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  })
+
+  toast.success('Transaction added!')
+}
+
+// Generate unique ID
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
 </script>
