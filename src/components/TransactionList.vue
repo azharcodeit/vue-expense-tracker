ccdf51fe3509d98669aa3a99d5175cbbde701641
Azhar Botbayeva <azharbotbayeva@Azhars-MacBook-Pro.local>
2024-01-05 15:43:02 +0000

    refactor:change from local to global state pass/receive props through defineProps

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,23 +1,16 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">{{ transaction.text }}
-            <span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            transactions: [
-                { id: 1, text: 'Flower', amount: -19.99 },
-                { id: 2, text: 'Salary', amount: 299.99 },
-                { id: 3, text: 'Book', amount: -10 },
-                { id: 4, text: 'Camera', amount: 150 },
-            ]
-        }
-    }
-}
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+    type: Array,
+    required: true
+})
 </script>
